f6e5fc5962e23439b806bc9897bc7bc9a6e00b0f
ruiiiiiiiiiiiiiiiiiiiiiiiiiii <ruibottom69@gmail.com>
2026-05-20 14:14:59 -0300

    add home route

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("API funcionando 🚀");
+});
+
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
