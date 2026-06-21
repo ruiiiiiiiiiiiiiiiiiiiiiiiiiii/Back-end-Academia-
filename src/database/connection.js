8bea882384b0415acc9c15ab21f67cb4cc28bc5b
ruiiiiiiiiiiiiiiiiiiiiiiiiiii <ruibottom69@gmail.com>
2026-06-21 19:23:14 -0300

    Atualizacao do projeto

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,16 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connection = await mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "academia",
-    port: 3306
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+    waitForConnections: true,
+    connectionLimit: 10
 });
 
 export default { connection };
